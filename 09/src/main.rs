579fe2e65c4e324d19eb6fed2d98b40edc4cfb08
Simon Oliver <simon@handcircus.com>
2024-12-09 17:53:50 +0000

    Pt2 - Working on test data

--- 09/src/main.rs
+++ 09/src/main.rs
@@ -3,8 +3,7 @@ use std::fs;
 fn create_disk_map(input:&str) -> Vec::<(i64,bool)>
 {
     let compressed_representation:Vec<u32>=input.chars()
-        .filter(|char| {char.is_numeric()})
-        .into_iter().map(|input_char|input_char.to_digit(10).unwrap()).collect();
+        .filter(|char| {char.is_numeric()}).map(|input_char|input_char.to_digit(10).unwrap()).collect();
     let mut disk_map:Vec<(i64,bool)>=Vec::new();
     let mut file_index=0;
     let mut is_file_data=true;
@@ -19,10 +18,9 @@ fn create_disk_map(input:&str) -> Vec::<(i64,bool)>
     disk_map
 }
 
-fn defrag_disk_data(vec_data : &mut Vec::<(i64,bool)>) {
+fn defrag_disk_data_pt1(vec_data : &mut Vec::<(i64,bool)>) {
     let mut dst_test_index=0;
     let mut defrag_src_index=(vec_data.len() as i32)-1;
-
     while defrag_src_index>dst_test_index
     {
         if vec_data[defrag_src_index as usize].1 {
@@ -34,6 +32,64 @@ fn defrag_disk_data(vec_data : &mut Vec::<(i64,bool)>) {
                     vec_data.swap(defrag_src_index as usize,dst_test_index as usize); // Swap
                 }
                 dst_test_index+=1; // Increment dst pos
+            }
+        }
+        defrag_src_index-=1; // Decrement src pos
+    }
+}
+
+fn defrag_disk_data_pt2(vec_data : &mut Vec::<(i64,bool)>) {
+
+    let mut defrag_src_index=(vec_data.len() as i32)-1;
+    while defrag_src_index>0 // Loop back to start
+    {
+        if vec_data[defrag_src_index as usize].1 {
+            let file_index=vec_data[defrag_src_index as usize].0;
+            let file_end_index=defrag_src_index;
+            let mut file_start_index=defrag_src_index;
+            let mut found_file_start=false;
+            while !found_file_start
+            {
+                if defrag_src_index<0 {
+                    file_start_index = defrag_src_index + 1; // Previous location is start pos
+                    found_file_start = true;
+                }
+                else {
+                    let block_data = vec_data[defrag_src_index as usize];
+                    if !block_data.1 || block_data.0 != file_index { // Either empty or diff index
+                        file_start_index = defrag_src_index + 1; // Previous location is start pos
+                        found_file_start = true;
+                        defrag_src_index+=1; // Increment back one to support next file start
+                    } else {
+                        defrag_src_index -= 1; // Decrement
+                    }
+                }
+            }
+            let file_length=(file_end_index+1)-file_start_index;
+            //println!("Moving file index {file_index} length {file_length} - start index {file_start_index}->{file_end_index}");
+            let mut dst_test_index=0; // We'll restart at 0 each time
+            let mut found_valid_dst=false;
+            while !found_valid_dst && dst_test_index<file_start_index
+            {
+                if !vec_data[dst_test_index as usize].1 { // Empty
+                    // Calc space
+                    let mut sufficient_space=true;
+                    // Check each location ahead also clear
+                    for size_test in 1..file_length {
+                        if vec_data[(dst_test_index+size_test) as usize].1 {
+                            sufficient_space=false;
+                        }
+                    }
+                    if sufficient_space {
+                        found_valid_dst = true; // End loop
+                        // Swap each location
+                        for size_index in 0..file_length {
+                            vec_data.swap((file_start_index + size_index) as usize, (dst_test_index + size_index) as usize); // Swap
+                        }
+                    }
+                    // TODO - Could also skip index forward a little as don't need to recheck
+                }
+                dst_test_index+=1; // Increment dst pos if not found
             }
         }
         defrag_src_index-=1; // Decrement src pos
@@ -60,9 +116,14 @@ fn print_vec(vec_data : &Vec::<(i64,bool)>)
 
 fn main() {
     let content=fs::read_to_string("data/test_input").expect("Expected to read file");
-    let mut disk_map=create_disk_map(&content);
-    print_vec(&disk_map);
-    defrag_disk_data(&mut disk_map);
-    print_vec(&disk_map);
-    println!("Pt1 - checksum {}",calc_checksum(&disk_map));
+    let mut disk_map_pt1=create_disk_map(&content);
+    let mut disk_map_pt2=disk_map_pt1.clone();
+    print_vec(&disk_map_pt1);
+    defrag_disk_data_pt1(&mut disk_map_pt1);
+    print_vec(&disk_map_pt1);
+    println!("Pt1 - checksum {}",calc_checksum(&disk_map_pt1));
+
+    defrag_disk_data_pt2(&mut disk_map_pt2);
+    print_vec(&disk_map_pt2);
+    println!("Pt2 - checksum {}",calc_checksum(&disk_map_pt2));
 }
