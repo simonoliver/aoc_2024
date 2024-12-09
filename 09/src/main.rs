d5bc1d62bb7e805e50ae10179f115808b2f81e5a
Simon Oliver <simon@handcircus.com>
2024-12-09 17:16:32 +0000

    pt1 - defrag working

--- 09/src/main.rs
+++ 09/src/main.rs
@@ -19,6 +19,28 @@ fn create_disk_map(input:&str) -> Vec::<(i64,bool)>
     disk_map
 }
 
+fn defrag_disk_data(vec_data : &mut Vec::<(i64,bool)>)
+{
+    let mut dst_test_index=0;
+    let mut defrag_src_index=(vec_data.len() as i32)-1;
+
+    while defrag_src_index>dst_test_index
+    {
+        if vec_data[defrag_src_index as usize].1 {
+            let mut found_valid_dst=false;
+            while !found_valid_dst && dst_test_index<defrag_src_index
+            {
+                if !vec_data[dst_test_index as usize].1 {
+                    found_valid_dst=true;
+                    vec_data.swap(defrag_src_index as usize,dst_test_index as usize); // Swap
+                }
+                dst_test_index+=1; // Increment dst pos
+            }
+        }
+        defrag_src_index-=1; // Decrement src pos
+    }
+}
+
 fn print_vec(vec_data : &Vec::<(i64,bool)>)
 {
     let mut output_string=String::new();
@@ -33,5 +55,7 @@ fn print_vec(vec_data : &Vec::<(i64,bool)>)
 fn main() {
     let content=fs::read_to_string("data/test_input").expect("Expected to read file");
     let mut disk_map=create_disk_map(&content);
+    print_vec(&disk_map);
+    defrag_disk_data(&mut disk_map);
     print_vec(&disk_map);
 }
