32ceff79b25e80e72bf3f33d9a46c84bb6858524
Simon Oliver <simon@handcircus.com>
2024-12-13 15:05:07 +0000

    Slight cleanup, working on pt2

--- 12/src/main.rs
+++ 12/src/main.rs
@@ -11,13 +11,12 @@ fn add_region_neighbours_at_location(garden_data:&Vec<Vec<char>>, region_locatio
     let current_index=region_location.0+region_location.1*(garden_data[0].len() as i32);
     processed_locations_list[current_index as usize]=true; // Mark as processed
 
-    for (direction_index,direction) in NEIGHBOUR_DIRECTIONS.iter().enumerate() {
+    for direction in NEIGHBOUR_DIRECTIONS.iter() {
         let test_location=(region_location.0+direction.0,region_location.1+direction.1);
         if test_location.0>=0 && test_location.0<(garden_data[0].len() as i32) && test_location.1>=0 && test_location.1<(garden_data.len() as i32) { // Bounds check
             let location_index=test_location.0+test_location.1*(garden_data[0].len() as i32);
             if garden_data[test_location.1 as usize][test_location.0 as usize]==garden_data[region_location.1 as usize][region_location.0 as usize] { // Char match
                 found_neighbours+=1;
-                //neighbors_bits|=1<<direction_index; // Add neighbor bit
                 if !processed_locations_list[location_index as usize] { // If not processed, add
                     add_region_neighbours_at_location(garden_data,test_location,found_locations,processed_locations_list);  // Recuse for the new location
                 }
@@ -28,13 +27,11 @@ fn add_region_neighbours_at_location(garden_data:&Vec<Vec<char>>, region_locatio
     for (direction_index,direction) in ALL_NEIGHBOUR_DIRECTIONS.iter().enumerate() {
         let test_location=(region_location.0+direction.0,region_location.1+direction.1);
         if test_location.0>=0 && test_location.0<(garden_data[0].len() as i32) && test_location.1>=0 && test_location.1<(garden_data.len() as i32) { // Bounds check
-            let location_index=test_location.0+test_location.1*(garden_data[0].len() as i32);
             if garden_data[test_location.1 as usize][test_location.0 as usize]==garden_data[region_location.1 as usize][region_location.0 as usize] { // Char match
                 neighbors_bits|=1<<(7-direction_index); // Add neighbor bit
             }
         }
     }
-    println!("Neighbors bits {neighbors_bits}");
     found_locations.push((region_location.0,region_location.1,found_neighbours,neighbors_bits)); // Add this location with neighbour count
 }
 
@@ -60,10 +57,6 @@ fn find_regions(garden_data:&Vec<Vec<char>>) -> Vec<Vec<(i32,i32,i32,u8)>> {
 
 fn side_count (side_bits:u8) -> u8
 {
-    // TODO Need to factor in diagonals, as will create a valid side if diagonal present but still
-    // an entry to the left
-
-    //[(1,0),(0,1),(-1,0),(0,-1)]; Right, down, left, top . RDLT
     let mut side_count:u8=0;
     //                    →↘↓↙←↖↑↗      →↘↓↙←↖↑↗
     if side_bits &      0b00001010 == 0b00000000 {side_count+=1} // Nothing above and left
@@ -78,16 +71,13 @@ fn side_count (side_bits:u8) -> u8
 }
 
 fn main() {
-    let content = fs::read_to_string("data/test_input").expect("Expected to read the file");
+    let content = fs::read_to_string("data/input").expect("Expected to read the file");
 
     let lines=content.split("\n").filter(|line|line.len()>0);
     let garden_data:Vec<Vec<char>>=lines.map(|line|{
         line.chars().collect()
     }).collect();
 
-    for line in &garden_data {
-        println!("Line {:?}",line);
-    }
     let found_regions=find_regions(&garden_data);
     let mut pt1_total=0;
     let mut pt2_total=0;
@@ -96,16 +86,8 @@ fn main() {
         let region_size=region.len();
         let region_borders=region.iter().fold(0,|acc,region_entry|acc+(4-region_entry.2));
         let side_count=region.iter().fold(0,|acc,region_entry|acc+side_count(region_entry.3));
-        let first_entry=region[0];
-        let first_char=garden_data[first_entry.1 as usize][first_entry.0 as usize];
-        println!("Region {} size {} borders {} side_count {}",first_char,region_size,region_borders,side_count);
         pt1_total+=region_size as i32*region_borders;
         pt2_total+=region_size as i32*(side_count as i32);
-    }
-    let region1=&found_regions[0];
-    for location in region1 {
-        let sides=side_count(location.3);
-        println!("Location {},{} {:b} side count {}",location.0,location.1,location.3,sides);
     }
     println!("Pt1 Total {pt1_total}");
     println!("Pt2 Total {pt2_total}");
