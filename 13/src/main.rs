53b309245ddcb4ef08cf4d80e7f9dca080f01ee6
Simon Oliver <simon@handcircus.com>
2024-12-13 16:10:12 +0000

    Pt1 done

--- 13/src/main.rs
+++ 13/src/main.rs
@@ -1,8 +1,24 @@
 use std::fs;
+use std::cmp;
 use regex::Regex;
 
 fn find_lowest_win_combo(a_button:(i32,i32),b_button:(i32,i32),prize_pos:(i32,i32)) -> (bool,i32) {
     println!("AButton {:?} BButton {:?} Prize is {:?}",a_button,b_button,prize_pos);
+    // We want to find the solution where a_count*a_button+b_count*b_button=prize_pos and
+    // A is 3 tokens, B is 1. So we want the smallest value of B while still satisfying it
+    let max_b= cmp::max(prize_pos.0/b_button.0,prize_pos.1/b_button.1); // max B before exceeding
+    for b_count in 0..max_b {
+        let mut exceeded=false;
+        let mut a_count=0;
+        while (!exceeded) {
+            let total=(b_button.0*b_count+a_button.0*a_count,b_button.1*b_count+a_button.1*a_count);
+            if total==prize_pos {
+                return (true,a_count*3+b_count);
+            }
+            if total.0>prize_pos.0 || total.1>prize_pos.1 {exceeded=true;}
+            a_count+=1;
+        }
+    }
     (false,0)
 }
 
@@ -19,7 +35,7 @@ fn get_data(contents:&str,regex_formula:&str) -> Vec<(i32,i32)>
 }
 
 fn main() {
-    let content = fs::read_to_string("data/test_input").expect("Expected to read the file");
+    let content = fs::read_to_string("data/input").expect("Expected to read the file");
 
     let button_a_entries:Vec<(i32,i32)>=get_data(&content,r"Button A: X\+([0-9]+), Y\+([0-9]+)");
     let button_b_entries:Vec<(i32,i32)>=get_data(&content,r"Button B: X\+([0-9]+), Y\+([0-9]+)");
