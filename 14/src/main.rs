3de4e986c681d3852e563c49099213516bf90439
Simon Oliver <simon@handcircus.com>
2024-12-15 09:33:44 +0000

    Grid print

--- 14/src/main.rs
+++ 14/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::ops::Add;
 use regex;
 use regex::Regex;
 
@@ -22,11 +23,28 @@ fn count_robots(location_data: &Vec<((i64, i64), (i64, i64))>, area_size: (i64, 
     )
 }
 
+fn print_grid(location_data: &Vec<((i64,i64),(i64,i64))>,area_size:(i64,i64)) {
+    let mut grid:Vec<Vec<i64>>=Vec::new();
+    for row in 0..area_size.1 {
+        grid.push(vec!(0;area_size.0 as usize));
+    }
+    for (index,location_data) in location_data.iter().enumerate() {
+       let location=location_data.0;
+        grid[location.1 as usize][location.0 as usize]+=1; // Append count
+    }
+    for row in grid {
+        let string_line=row.iter().fold(String::new(),|acc,value|acc.add(
+            &value.to_string()
+            //if *value==0 {"."} else {let val_string=value.to_string();&val_string}
+        ));
+        println!("{string_line}");
+    }
+}
 
 fn main() {
     let content = fs::read_to_string("data/test_input").expect("Expected to read the file");
     let regex=Regex::new(r"p=(-*[0-9]+),(-*[0-9]+) v=(-*[0-9]+),(-*[0-9]+)").unwrap();
-    let area_size:(i64,i64)=(11,7); // (101,103)
+    let area_size:(i64,i64)= (11,7); //(101,103); //
     let mut location_data:Vec<((i64,i64),(i64,i64))>= regex.captures_iter(&content)
         .filter(|capture|capture.get(0).unwrap().as_str().len()>0)
         .map(|capture| {
@@ -36,7 +54,8 @@ fn main() {
     println!("Data size {}",location_data.len());
 
     process_movement(&mut location_data,area_size,100);
-    println!("Pt1 - {}",count_robots(&location_data,area_size))
+    println!("Pt1 - {}",count_robots(&location_data,area_size));
+    print_grid(&location_data,area_size);
 }
 
 
