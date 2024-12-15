b08f7a7daa6c2bcb64c0bf6527f47c99f0647e8a
Simon Oliver <simon@handcircus.com>
2024-12-15 09:59:24 +0000

    Quadrant count properly

--- 14/src/main.rs
+++ 14/src/main.rs
@@ -15,12 +15,32 @@ fn process_movement (location_data: &mut Vec<((i64,i64),(i64,i64))>,area_size:(i
     }
 }
 
+fn get_quadrant_index(position:(i64,i64),area_size: (i64, i64)) -> i64 {
+    let mid_point=((area_size.0-1)/2,(area_size.1-1)/2);
+    if position.0<mid_point.0 {
+        if position.1<mid_point.1 {return 0;}
+        if position.1>mid_point.1 {return 1;}
+    }
+    if position.0>mid_point.0 {
+        if position.1<mid_point.1 {return 2;}
+        if position.1>mid_point.1 {return 3;}
+    }
+    -1 // Not in a quadrant
+}
+
 fn count_robots(location_data: &Vec<((i64, i64), (i64, i64))>, area_size: (i64, i64)) -> i64 {
-    location_data.iter().fold(0,
-        |acc,location| {
-            if location.0.0==(area_size.0-1)/2 || location.0.1==(area_size.1-1)/2 {acc} else {acc+1}
+    let quadrants:Vec<i64>=
+    location_data.iter().fold(vec!(0;4),
+        |mut acc,location| {
+            let quadrant=get_quadrant_index((location.0.0,location.0.1),area_size);
+            if (quadrant>=0) {acc[quadrant as usize]+=1;}
+            acc
+            //if quadrant<0 {acc}
+            //else acc[quadrant]+=1;acc
+            //if location.0.0==(area_size.0-1)/2 || location.0.1==(area_size.1-1)/2 {acc} else {acc+1}
         }
-    )
+    );
+    quadrants[0]*quadrants[1]*quadrants[2]*quadrants[3]
 }
 
 fn print_grid(location_data: &Vec<((i64,i64),(i64,i64))>,area_size:(i64,i64)) {
@@ -32,11 +52,15 @@ fn print_grid(location_data: &Vec<((i64,i64),(i64,i64))>,area_size:(i64,i64)) {
        let location=location_data.0;
         grid[location.1 as usize][location.0 as usize]+=1; // Append count
     }
-    for row in grid {
-        let string_line=row.iter().fold(String::new(),|acc,value|acc.add(
-            &value.to_string()
-            //if *value==0 {"."} else {let val_string=value.to_string();&val_string}
-        ));
+
+    for (row_index,row) in grid.iter().enumerate() {
+        let mut string_line=String::new();
+        for (column_index,value) in row.iter().enumerate() {
+            if row_index==(area_size.1 as usize-1)/2 {string_line.push(' ')}
+            else if column_index==(area_size.0 as usize-1)/2 {string_line.push(' ')}
+            else if *value==0 {string_line.push('.');}
+            else {string_line.push_str(value.to_string().as_str());}
+        }
         println!("{string_line}");
     }
 }
